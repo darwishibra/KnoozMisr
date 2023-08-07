50bf9c881c3d695f2481a257447205ae93a0bab4
darwishibra <98791805+darwishibra@users.noreply.github.com>
2023-08-07 17:30:19 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
