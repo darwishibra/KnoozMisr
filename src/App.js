cd65cf1d2171a15362957e9f6fb60e3005483213
darwishibra <98791805+darwishibra@users.noreply.github.com>
2023-08-07 18:56:31 -0400

    Configured use react router dom the default page of the web application to be the home page, and linked the /home path to the home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-
+          <Route index element = {<Home />} />
+          <Route path = "/home" element = {<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
